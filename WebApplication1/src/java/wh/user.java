220907032837bf92f02972ab29d6ec053a8321fd
kamalbaktir <kamalbaktir@gmail.com>
2011-11-24 04:36:45 +0000

    Upload dari Achmad Kamal 5208100137 script coding dari use case order jasa

---
+++ WebApplication1/src/java/wh/user.java
@@ -0,0 +1,81 @@
+package wh;
+
+/**
+ *
+ * @author Achmad
+ */
+public class user {
+    private String username;
+    private String password;
+    private String nama;
+    private String notelp;
+    private String alamat;
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the notelp
+     */
+    public String getNotelp() {
+        return notelp;
+    }
+
+    /**
+     * @param notelp the notelp to set
+     */
+    public void setNotelp(String notelp) {
+        this.notelp = notelp;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
