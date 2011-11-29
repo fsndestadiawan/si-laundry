759b88e8d0c8faebd931a2e32446340aa3d285c5
kamalbaktir <kamalbaktir@gmail.com>
2011-11-29 02:10:56 +0000

    Achmad Kamal 5208100137 menambahkan order.jsp tmborder.jsp order.java dan menambahkan class tambahorder di method.java

---
+++ RBPL.SI.Laundry/src/java/Laundry/order.java
@@ -0,0 +1,70 @@
+package Laundry;
+
+/**
+ *
+ * @author Achmad
+ */
+public class order {
+    private String noorder;
+    private String paketorder;
+    private String waktuambil;
+    private String alamatambil;
+
+    /**
+     * @return the noorder
+     */
+    public String getNoorder() {
+        return noorder;
+    }
+
+    /**
+     * @param noorder the noorder to set
+     */
+    public void setNoorder(String noorder) {
+        this.noorder = noorder;
+    }
+
+    /**
+     * @return the paketorder
+     */
+    public String getPaketorder() {
+        return paketorder;
+    }
+
+    /**
+     * @param paketorder the paketorder to set
+     */
+    public void setPaketorder(String paketorder) {
+        this.paketorder = paketorder;
+    }
+
+    /**
+     * @return the waktuambil
+     */
+    public String getWaktuambil() {
+        return waktuambil;
+    }
+
+    /**
+     * @param waktuambil the waktuambil to set
+     */
+    public void setWaktuambil(String waktuambil) {
+        this.waktuambil = waktuambil;
+    }
+
+    /**
+     * @return the alamatambil
+     */
+    public String getAlamatambil() {
+        return alamatambil;
+    }
+
+    /**
+     * @param alamatambil the alamatambil to set
+     */
+    public void setAlamatambil(String alamatambil) {
+        this.alamatambil = alamatambil;
+    }
+
+}
+
