895f74085241dc3eb441967a4f7720f1d44b35ab
prince.andalusia <prince.andalusia@gmail.com>
2012-01-04 14:18:14 +0000

    menambahkan notifikasi.java

---
+++ RBPL.SI.Laundry/src/java/Laundry/notifikasi.java
@@ -0,0 +1,83 @@
+package Laundry;
+
+/**
+ *
+ * @author Arief
+ */
+public class notifikasi {
+    private String paketorder;
+    private String waktuambil;
+    private String alamatambil;
+    private String tagihan;
+    private String reward;
+
+    /**
+     * @return the paketorder
+     */
+    public String getPaketorder() {
+        return paketorder;
+    }
+
+    /**
+     * @param paketorder the paketorder to set
+     */
+    public void setPaketorder(String paketorder) {
+        this.paketorder = paketorder;
+    }
+
+    /**
+     * @return the waktuambil
+     */
+    public String getWaktuambil() {
+        return waktuambil;
+    }
+
+    /**
+     * @param waktuambil the waktuambil to set
+     */
+    public void setWaktuambil(String waktuambil) {
+        this.waktuambil = waktuambil;
+    }
+
+    /**
+     * @return the alamatambil
+     */
+    public String getAlamatambil() {
+        return alamatambil;
+    }
+
+    /**
+     * @param tagihan the tagihan to set
+     */
+    public void setAlamatambil(String alamatambil) {
+        this.alamatambil = alamatambil;
+    }
+/**
+     * @return the tagihan
+     */
+    public String getTagihan() {
+        return tagihan;
+    }
+
+    /**
+     * @param tagihan the tagihan to set
+     */
+    public void setTagihan(String tagihan) {
+        this.tagihan = tagihan;
+    }
+
+    /**
+     * @return the reward
+     */
+    public String getReward() {
+        return reward;
+    }
+
+    /**
+     * @param reward the reward to set
+     */
+    public void setReward(String reward) {
+        this.reward = reward;
+    }
+}
+
