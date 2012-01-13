c9b8ec4402890a6afa73f83786b2c91b360a5f6e
prince.andalusia@gmail.com <prince.andalusia@gmail.com@0f5ca294-dd9c-681c-039b-a40408217eb1>
2012-01-13 13:27:16 +0000

    mengubah source code notifikasi.java

--- trunk/RBPL.SI.Laundry/src/java/Laundry/notifikasi.java
+++ trunk/RBPL.SI.Laundry/src/java/Laundry/notifikasi.java
@@ -1,83 +1,142 @@
-package Laundry;
+package org.opennms.web.notification.bobject;
 
-/**
- *
- * @author Arief
- */
-public class notifikasi {
-    private String paketorder;
-    private String waktuambil;
-    private String alamatambil;
-    private String tagihan;
-    private String reward;
+        import java.util.ArrayList;
+        import java.util.List;
 
-    /**
-     * @return the paketorder
-     */
-    public String getPaketorder() {
-        return paketorder;
-    }
+        import org.opennms.web.WebSecurityUtils;
 
-    /**
-     * @param paketorder the paketorder to set
-     */
-    public void setPaketorder(String paketorder) {
-        this.paketorder = paketorder;
-    }
+        /**
+         * This class holds the information parsed from the notifications.xml
+         * 
+         * author: Arief
+         * @version 1.1.1.1
+         */
+        public class Notification {
+            /**
+             * The interval to wait between processing target
+             */
+            private String m_interval;
 
-    /**
-     * @return the waktuambil
-     */
-    public String getWaktuambil() {
-        return waktuambil;
-    }
+            /**
+             * The name of the notification
+             */
+            private String m_name;
 
-    /**
-     * @param waktuambil the waktuambil to set
-     */
-    public void setWaktuambil(String waktuambil) {
-        this.waktuambil = waktuambil;
-    }
+            /**
+             * Comments for the notification
+             */
+            private String m_comments;
 
-    /**
-     * @return the alamatambil
-     */
-    public String getAlamatambil() {
-        return alamatambil;
-    }
+            /**
+             * The list of users or other notifications to include in this notification
+             */
+            private List<NotificationTarget> m_targets;
 
-    /**
-     * @param tagihan the tagihan to set
-     */
-    public void setAlamatambil(String alamatambil) {
-        this.alamatambil = alamatambil;
-    }
-/**
-     * @return the tagihan
-     */
-    public String getTagihan() {
-        return tagihan;
-    }
+            /**
+             * Default constructor, initializes members
+             */
+            public Notification() {
+                m_targets = new ArrayList<NotificationTarget>();
+            }
 
-    /**
-     * @param tagihan the tagihan to set
-     */
-    public void setTagihan(String tagihan) {
-        this.tagihan = tagihan;
-    }
+            /**
+             * Sets the name of the notification
+             * 
+             * @param name
+             *            the name to be set for this notification.
+             */
+            public void setName(String name) {
+                m_name = name;
+            }
 
-    /**
-     * @return the reward
-     */
-    public String getReward() {
-        return reward;
-    }
+            /**
+             * Returns the name of the notification
+             * 
+             * @return the name of the notification.
+             */
+            public String getName() {
+                return m_name;
+            }
 
-    /**
-     * @param reward the reward to set
-     */
-    public void setReward(String reward) {
-        this.reward = reward;
-    }
-}
+            /**
+             * Sets the comments for the notification
+             * 
+             * @param comments
+             *            comments to be set for this notification.
+             */
+            public void setComments(String comments) {
+                m_comments = comments;
+            }
 
+            /**
+             * Returns the comments for the notification
+             * 
+             * @return comments for this notification.
+             */
+            public String getComments() {
+                return m_comments;
+            }
+
+            /**
+             * Sets the interval for the notification
+             * 
+             * @param interval
+             *            the interval to be set for this notification.
+             */
+            public void setInterval(String interval) {
+                m_interval = interval;
+            }
+
+            /**
+             * Returns the string version of the interval
+             * 
+             * @return the interval for this notification.
+             */
+            public String getInterval() {
+                return m_interval;
+            }
+
+            /**
+             * Returns the interval converted to milliseconds
+             * 
+             * @return the interval in milliseconds
+             */
+            public long getIntervalMilliseconds() {
+                long interval = 0;
+
+                if (!m_interval.equals("all")) {
+                    // interval = TimeConverter.convertToMillis(m_interval);
+                    interval = WebSecurityUtils.safeParseInt(m_interval);
+                }
+
+                return interval;
+            }
+
+            /**
+             * Returns the interval in seconds
+             * 
+             * @return the interval in seconds
+             */
+            public long getIntervalSeconds() {
+                return getIntervalMilliseconds() / 1000;
+            }
+
+            /**
+             * Adds a target to the notification
+             * 
+             * @param target
+             *            a target to be added for this notification.
+             */
+            public void addTarget(NotificationTarget target) {
+                m_targets.add(target);
+            }
+
+            /**
+             * Returns the list of targets
+             * 
+             * @return the list of targets.
+             */
+            public List<NotificationTarget> getTargets() {
+                return m_targets;
+            }
+        }
